b9df68c8435a47ac7cf33966e2298c3ffa64a270
Eriton <botheritons2005@gmail.com>
2024-10-17 19:28:33 -0300

    feat: putting names on routes

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,17 @@ use App\Http\Middleware\JwtAuthMiware; // Atualizado para o nome correto do midd
 // Aplicando o middleware JWT nas rotas protegidas
 Route::middleware([JwtAuthMiware::class])->group(function () {
     // Rota para obter informações do usuário autenticado
-    Route::get('/user/{id}', [UserController::class, 'show']); // Chama o método show do UserController
+    Route::get('/user/{id}', [UserController::class, 'show'])
+        ->name('user.find.id'); // Chama o método show do UserController
 
     // Rota para criar um novo usuário
-    Route::post('/users', [UserController::class, 'store']);
-
+    Route::post('/users', [UserController::class, 'store'])
+        ->name('user.find.all');
     // Rota para logout
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout'])
+        ->name('user.logout');
 });
 
 // Rota pública para login
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('user.login');
